5628f813a4f24472a14ab2441bc45e0e8aa42b57
SujayDas2002 <Sujay.Das2@cognizant.com>
2026-05-08 16:55:48 +0530

    Configuration done

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,4 +1,17 @@
 package steps;
 
+import base.BaseTest;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+// Takes a screenshot after every Cucumber scenario and embeds it in the report.
 public class Hooks {
+
+    @After
+    public void afterScenario(Scenario scenario) {
+        byte[] png = ((TakesScreenshot) BaseTest.driver).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(png, "image/png", scenario.getName());
+    }
 }
