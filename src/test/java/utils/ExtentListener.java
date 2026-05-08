199b2357ea5ac6d9c55779c881784ef6775efd8a
Lanjewar <2478504@cognizant.com>
2026-05-08 15:59:34 +0530

    configuration file completed

--- src/test/java/utils/ExtentListener.java
+++ src/test/java/utils/ExtentListener.java
@@ -1,4 +1,17 @@
 package utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.SportsPage;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class ExtentListener {
 }
